0e6da2e2118b0682f25dc32b8bd1132d4774b061
Merazz <83541936+MerazMz@users.noreply.github.com>
2026-03-12 22:20:45 +0530

    Time: 3 ms (69.88%), Space: 12.8 MB (96.26%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rec(int n, string& s,int open, int close,vector<string>& ans){
+        if(s.size()==2*n){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            s.push_back('(');
+            rec(n,s,open+1,close,ans);
+            s.pop_back();
+        }
+        if(close<open){
+            s.push_back(')');
+            rec(n,s,open,close+1,ans);
+            s.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s;
+        rec(n,s,0,0,ans);
+        return ans;
+    }
+};
